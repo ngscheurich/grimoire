b2e295718a5eb5f83bca81850dcab04f7515d749
N. G. Scheurich <nick@scheurich.me>
2025-09-03 15:35:21 -0500

    refactor: formatting and variable casing

--- exercise.sh
+++ exercise.sh
@@ -9,11 +9,11 @@
 # Flags:
 #	Additional flage to pass to the exercism CLI.
 #
-#  Creates a new tmux window for the exercise. It is split vertically
-#  into two panes:
+# Creates a new tmux window for the exercise. It is split vertically into two
+# panes:
 #
-#  1. 70% height; Neovim with README.md and source file tab pages
-#  2. 30% height; test command
+# 1. 70% height; Neovim with README.md and source file tab pages
+# 2. 30% height; test command
 #
 
 set -euo pipefail
@@ -23,19 +23,19 @@ source "${XDG_DATA_HOME:-$HOME/.local/share}/grimoire/utils.sh"
 
 ensure "gum"
 
-TRACK="$1"
+track="$1"
 shift
-EXERCISE="$1"
+exercise="$1"
 shift
 
-EXERCISE_DIR="$HOME/Exercism/$TRACK/$EXERCISE"
+EXERCISE_DIR="$HOME/Exercism/$track/$exercise"
 WINNAME="{} $(basename "$EXERCISE_DIR")"
 
 if [ ! -d "$EXERCISE_DIR" ]; then
-	gum spin --title="Downloading $TRACK/$EXERCISE..." -- \
+	gum spin --title="Downloading $track/$exercise..." -- \
 		exercism download \
-		--track "$TRACK" \
-		--exercise "$EXERCISE" \
+		--track "$track" \
+		--exercise "$exercise" \
 		"$@"
 fi
 
@@ -45,7 +45,7 @@ ls
 files="README.md"
 declare testcmd
 
-case "$TRACK" in
+case "$track" in
 gleam)
 	files="$files $(find src -type f | head -n 1)"
 	testcmd="watchexec -c -w src gleam test"
